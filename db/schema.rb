aed1510f47dccdb4ebc2dd579499422079fd022e
SNMellott <snmellott@gmail.com>
2020-05-05 21:58:29 -0400

    Finally got the admin account working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_231938) do
+ActiveRecord::Schema.define(version: 2020_05_05_231911) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_231938) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
